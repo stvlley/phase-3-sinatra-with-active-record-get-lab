fb7d33ed4de05820b0e88e3d63814b6f14289c44
Stephen Talley <stvlley.amzn@gmail.com>
2022-03-26 15:34:18 -0400

    commit tree a15a881bc973f45baefe9c50826a2d09091e0998

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set default_content_type: "application/json"
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    resp = Bakery.find(params[:id])
+    resp.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    resp = BakedGood.all.sort_by(&:price).reverse
+    resp.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    resp = BakedGood.all.sort_by(&:price).reverse.first
+    resp.to_json
+  end
+
 
 end
